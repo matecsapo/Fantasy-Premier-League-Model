d66c57a6d80e7eddfcaaebf4c2c2ead27e3f8fd6
Máté Csapo <matecsapo@Mates-MacBook-Pro.local>
2025-08-29 23:58:15 -0400

    Added remote data pulling option

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+LOCAL_LOCATION = "FPL-Elo-Insights"
+
+REMOTE_LOCATION = "https://raw.githubusercontent.com/olbauday/FPL-Elo-Insights/main"
+
+DATA_SOURCE = LOCAL_LOCATION
+
 # Loads:    player_id | name | team | ...
 # order by player_id
 def load_players(season):
@@ -7,7 +13,7 @@ def load_players(season):
     if season == "2024-2025":
         location = "FPL-Elo-Insights/data/2024-2025/players/players.csv"
     elif season == "2025-2026":
-        location = "FPL-Elo-Insights/data/2025-2026/players.csv"
+        location = f"{DATA_SOURCE}/data/2025-2026/players.csv"
     players = pd.read_csv(location)
     players = players.sort_values(by="player_id")
     return players
@@ -22,7 +28,7 @@ def load_stats(season, start_gw, end_gw):
         folder = "By Tournament/Premier League"
     gw_stats = []
     for gw in range(start_gw, end_gw + 1):
-        this_gw_stats = pd.read_csv(f"FPL-Elo-Insights/data/{season}/{folder}/GW{gw}/playermatchstats.csv")
+        this_gw_stats = pd.read_csv(f"{DATA_SOURCE}/data/{season}/{folder}/GW{gw}/playermatchstats.csv")
         this_gw_stats["gw"] = gw
         gw_stats.append(this_gw_stats)
     stats = pd.concat(gw_stats, ignore_index=True)
@@ -36,7 +42,7 @@ def load_FPL_info(season, start_gw, end_gw):
     if season == "2024-2025":
         location = "FPL-Elo-Insights/data/2024-2025/playerstats/playerstats.csv"
     elif season == "2025-2026":
-        location = "FPL-Elo-Insights/data/2025-2026/playerstats.csv"
+        location = f"{DATA_SOURCE}/data/2025-2026/playerstats.csv"
     FPL_info = pd.read_csv(location)
     FPL_info = FPL_info[FPL_info["gw"].isin(range(start_gw, end_gw + 1))]
     FPL_info = FPL_info.sort_values(by=["id", "gw"]).reset_index(drop=True)
@@ -48,7 +54,7 @@ def load_teams_info(season):
     if season == "2024-2025":
         location = "FPL-Elo-Insights/data/2024-2025/teams/teams.csv"
     elif season == "2025-2026":
-        location = "FPL-Elo-Insights/data/2025-2026/teams.csv"
+        location = f"{DATA_SOURCE}/data/2025-2026/teams.csv"
     teams_info = pd.read_csv(location)
     return teams_info
 
@@ -63,7 +69,7 @@ def load_fixture_info(season, start_gw, end_gw):
     elif season == "2025-2026":
         gw_matches = []
         for gw in range(start_gw, end_gw + 1):
-            this_gw_matches = pd.read_csv(f"FPL-Elo-Insights/data/2025-2026/By Tournament/Premier League/GW{gw}/matches.csv")
+            this_gw_matches = pd.read_csv(f"{DATA_SOURCE}/data/2025-2026/By Tournament/Premier League/GW{gw}/matches.csv")
             gw_matches.append(this_gw_matches)
         matches = pd.concat(gw_matches, ignore_index=True)
         matches = matches.sort_values(by="gameweek")
