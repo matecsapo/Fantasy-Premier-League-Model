88784181328d0ce98e986a072f987a73f1da4abe
Máté Csapo <matecsapo@Mates-MacBook-Pro.local>
2025-10-27 14:08:14 -0400

    GW10 Predictions - 10/27/2025/14:10EST

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import threading
 import time
 
 # Current gameweek we are predicting for
-CURRENT_GAMEWEEK = 9
+CURRENT_GAMEWEEK = 10
 
 # Set our data source as remote
 data_processing.DATA_SOURCE = data_processing.REMOTE_LOCATION
