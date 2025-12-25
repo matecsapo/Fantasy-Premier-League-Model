8d3eef7b46339a2924e55214ae6ef9a7dfc67fbd
Máté Csapo <matecsapo@Mates-MacBook-Pro.local>
2025-12-25 16:16:20 -0500

    GW18 Predictions - 12/25/2025/16:12EST

--- predict.py
+++ predict.py
@@ -125,7 +125,6 @@ def pick_11(predictions):
         prob += pulp.lpSum([x[pid] for pid in pos_players]) >= min_p
         prob += pulp.lpSum([x[pid] for pid in pos_players]) <= max_p
     # Budget constraint --> 83M... 17M = min to create bench!
-    predictions[predictions["now_cost"].isna()].to_csv("test.csv")
     prob += pulp.lpSum([
         predictions.loc[predictions['player_id']==pid, 'now_cost'].values[0] * x[pid] 
         for pid in players
